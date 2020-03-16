12828884f6137da15a6dc69f595c7fc0964641fc
Chattox <conradlineker@gmail.com>
2020-03-16 13:03:25 +0000

    Tweaks to full article display

--- src/components/FullArticle.jsx
+++ src/components/FullArticle.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 
 const FullArticle = props => {
-  const { title, body, votes, topic, author, created_at } = props.article;
+  const {
+    title,
+    body,
+    votes,
+    topic,
+    author,
+    created_at,
+    comment_count
+  } = props.article;
   return (
     <div className="FullArticle">
       <h1>{title}</h1>
       <h3>
         <i>Written by {author}</i>
       </h3>
+      <h5>
+        <i>in {topic}</i>
+      </h5>
       <br />
       <p>{body}</p>
+      <p>
+        <i>
+          Upvotes: {votes} Comments: {comment_count}
+        </i>
+      </p>
     </div>
   );
 };
